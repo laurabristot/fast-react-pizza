286fa590121cf7697caa6873e224aecf82e56466
laura bristot <laurabristotbarbi@hotmail.com>
2023-12-20 11:30:15 -0300

    9 - Fetching Orders

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Cart from './features/cart/Cart'
 import Menu, { loader as menuLoader } from './features/menu/Menu'
 import CreateOrder from './features/order/CreateOrder'
-import Order from './features/order/Order'
+import Order, { loader as orderLoader } from './features/order/Order'
 import AppLayout from './ui/AppLayout'
 import Error from './ui/Error'
 import Home from './ui/Home'
@@ -30,8 +30,10 @@ const router = createBrowserRouter([
         element: <CreateOrder />
       },
       {
-        path: '/order/:orderID',
-        element: <Order />
+        path: '/order/:orderId',
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />
       }
     ]
   }
