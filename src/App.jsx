03576349d36a0dbd56287f5688355a6a626528ce
laura bristot <laurabristotbarbi@hotmail.com>
2023-12-20 10:52:39 -0300

    8 - Handling Errors With Error Elements

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Menu, { loader as menuLoader } from './features/menu/Menu'
 import CreateOrder from './features/order/CreateOrder'
 import Order from './features/order/Order'
 import AppLayout from './ui/AppLayout'
+import Error from './ui/Error'
 import Home from './ui/Home'
 
 const router = createBrowserRouter([
@@ -17,6 +18,7 @@ const router = createBrowserRouter([
       {
         path: '/menu',
         element: <Menu />,
+        errorElement: <Error />,
         loader: menuLoader
       },
       {
