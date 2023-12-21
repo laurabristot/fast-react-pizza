2e0a519b0e69ecc417eef0b64efc56339e33d52c
laura bristot <laurabristotbarbi@hotmail.com>
2023-12-21 11:51:50 -0300

    25 - Styling the Menu

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,21 +1,21 @@
-import { useLoaderData } from 'react-router-dom'
-import { getMenu } from '../../services/apiRestaurant'
-import MenuItem from '../menu/MenuItem'
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "../menu/MenuItem";
 
 function Menu() {
-  const menu = useLoaderData()
+  const menu = useLoaderData();
   return (
-    <ul>
+    <ul className="divide-y divide-stone-200 px-2">
       {menu.map((pizza) => (
         <MenuItem pizza={pizza} key={pizza.id} />
       ))}
     </ul>
-  )
+  );
 }
 
 export async function loader() {
-  const menu = await getMenu()
-  return menu
+  const menu = await getMenu();
+  return menu;
 }
 
-export default Menu
+export default Menu;
