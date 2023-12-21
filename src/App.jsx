323656e3848ce1970576a59c7a66d3ebe31e5409
laura bristot <laurabristotbarbi@hotmail.com>
2023-12-21 11:24:09 -0300

    22 - Reusing Styles With React Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,52 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Cart from './features/cart/Cart'
-import Menu, { loader as menuLoader } from './features/menu/Menu'
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Cart from "./features/cart/Cart";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import CreateOrder, {
-  action as createOrderAction
-} from './features/order/CreateOrder'
-import Order, { loader as orderLoader } from './features/order/Order'
-import AppLayout from './ui/AppLayout'
-import Error from './ui/Error'
-import Home from './ui/Home'
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
+import Order, { loader as orderLoader } from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
+import Home from "./ui/Home";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
-        path: '/',
-        element: <Home />
+        path: "/",
+        element: <Home />,
       },
       {
-        path: '/menu',
+        path: "/menu",
         element: <Menu />,
         errorElement: <Error />,
-        loader: menuLoader
+        loader: menuLoader,
       },
       {
-        path: '/cart',
-        element: <Cart />
+        path: "/cart",
+        element: <Cart />,
       },
       {
-        path: '/order/new',
+        path: "/order/new",
         element: <CreateOrder />,
-        action: createOrderAction
+        action: createOrderAction,
       },
       {
-        path: '/order/:orderId',
+        path: "/order/:orderId",
         element: <Order />,
         loader: orderLoader,
-        errorElement: <Error />
-      }
-    ]
-  }
-])
+        errorElement: <Error />,
+      },
+    ],
+  },
+]);
 
 export default function App() {
   return (
     <RouterProvider router={router}>
       <div>hello vite</div>
     </RouterProvider>
-  )
+  );
 }
