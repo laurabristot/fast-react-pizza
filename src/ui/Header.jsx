2669a667c57b5953593ad9fff436e0df7ad02eb0
laura bristot <laurabristotbarbi@hotmail.com>
2023-12-20 23:22:38 -0300

    13 - Working With Color

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,11 +1,11 @@
-import { Link } from 'react-router-dom'
-import SearchOrder from '../features/order/SearchOrder'
+import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 export default function Header() {
   return (
-    <header>
+    <header className="bg-yellow-500">
       <Link to="/">Fast React Pizza Co.</Link>
       <SearchOrder />
     </header>
-  )
+  );
 }
